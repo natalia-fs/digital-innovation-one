32784de5fbd84e94425fad2816f85f1990e2a1c7
Natalia <natalia-321s@outlook.com>
2021-02-07 22:28:41 -0300

    (snake game) Ajustes de layout

--- snake-game/script.js
+++ snake-game/script.js
@@ -48,7 +48,7 @@ function startGame(){
   for(i=1; i< snake.length; i++){
     if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
       clearInterval(jogo);
-      alert("Game Over!")
+      alert("Game Over!");
     }
   }
 
