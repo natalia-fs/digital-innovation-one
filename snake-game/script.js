5ee7f6a4210318a0b1f740e378b5015e5933fdbc
Natalia <natalia-321s@outlook.com>
2021-02-07 22:16:05 -0300

    (snake game) Incrementando tamanho da cobrinha

--- snake-game/script.js
+++ snake-game/script.js
@@ -3,7 +3,7 @@ let context = canvas.getContext("2d");
 let box = 32;
 let snake = []
 snake[0] = {
-  x: 8 * box,
+  x: 2 * box,
   y: 8 * box
 }
 let direction = "right";
@@ -40,10 +40,10 @@ function update(event){
 
 function startGame(){
 
-  if(snake[0].x > 15 * box && direction === "right") snake[0].x = 0;
-  if(snake[0].x < 0 && direction === "left") snake[0].x = 16 * box;
-  if(snake[0].y > 15 * box && direction === "down") snake[0].y = 0;
-  if(snake[0].y < 0 && direction === "up") snake[0].y = 16 * box;
+  if(snake[0].x > 15 * box) snake[0].x = 0;
+  if(snake[0].x < 0) snake[0].x = 15 * box;
+  if(snake[0].y > 15 * box) snake[0].y = 0;
+  if(snake[0].y < 0) snake[0].y = 15 * box;
 
   createBG();
   createSnake();
@@ -57,8 +57,13 @@ function startGame(){
   if(direction === "up") snakeY -= box;
   if(direction === "down") snakeY += box;
 
-  snake.pop();
-
+  if(snakeX !== food.x || snakeY != food.y){
+    snake.pop();
+  } else {
+    food.x = Math.floor(Math.random() * 15 + 1) * box;
+    food.y = Math.floor(Math.random() * 15 + 1) * box;
+  }
+  
   let newHead = {
     x: snakeX,
     y: snakeY
