0f33026aa0f286b9adfbdd4dce95f821608da71e
Natalia <natalia-321s@outlook.com>
2021-02-07 21:04:56 -0300

    (snake game) Criação da cobrinha e movimento inicial

--- snake-game/script.js
+++ snake-game/script.js
@@ -1,10 +1,46 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
-
-function createBG() {
-    context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box);
+let snake = []
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box
 }
 
-createBG();
+let direction = "right";
+
+function createBG() {
+  context.fillStyle = "lightgreen";
+  context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function createSnake() {
+  for(i=0; i< snake.length; i++){
+    context.fillStyle = "green";
+    context.fillRect(snake[i].x, snake[i].y, box, box);
+  }
+}
+
+function startGame(){
+  createBG();
+  createSnake();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+  
+  if(direction === "right") snakeX += box;
+  if(direction === "left") snakeX -= box;
+  if(direction === "up") snakeY += box;
+  if(direction === "down") snakeY -= box;
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+  snake.unshift(newHead);
+  
+}
+
+let jogo = setInterval(startGame, 100);
